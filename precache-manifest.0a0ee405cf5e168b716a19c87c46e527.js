b56474caa6f669bedb375305cbc3926df3684e4f
Rotem Kellner <rkellner@augury.com>
2020-04-15 22:13:51 +0300

    Updates

---
+++ precache-manifest.0a0ee405cf5e168b716a19c87c46e527.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eab9e5ff7eca9e2c7087fa4b668b4d3",
+    "url": "/decision-support-tool/index.html"
+  },
+  {
+    "revision": "d42c10e7099a01557c67",
+    "url": "/decision-support-tool/static/js/2.fd0ecd42.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/decision-support-tool/static/js/2.fd0ecd42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9bf58dae5d47c0cb44a",
+    "url": "/decision-support-tool/static/js/3.6caa9c66.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/decision-support-tool/static/js/3.6caa9c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82176379d9aaa003ba62",
+    "url": "/decision-support-tool/static/js/4.3bff7bd8.chunk.js"
+  },
+  {
+    "revision": "cd0f494120d997755667",
+    "url": "/decision-support-tool/static/js/main.96e1bfa2.chunk.js"
+  },
+  {
+    "revision": "4698b9f0b178608fb192",
+    "url": "/decision-support-tool/static/js/runtime-main.d56b11fb.js"
+  }
+]);
