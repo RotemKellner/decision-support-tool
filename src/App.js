a846e611de4e1fe74070e16119d3fc1294badefe
Rotem Kellner <rkellner@augury.com>
2020-04-16 10:37:17 +0300

    return sw

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import theme from "./theme";
 import GlobalStyles from "./GlobalStyles";
+import * as serviceWorker from "./serviceWorker";
 import ReactGA from 'react-ga';
 
 const LoggedInComponent = lazy(() => import("./logged_in/components/Main"));
@@ -28,5 +29,7 @@ function App() {
     </BrowserRouter>
   );
 }
+
+serviceWorker.register();
 
 export default App;
