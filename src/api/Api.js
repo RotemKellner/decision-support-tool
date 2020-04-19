5c4f125661876c4dd222ce97b4a9824d5ed866a3
Matan Zutta <matanzu@amazon.com>
2020-04-19 14:49:10 +0300

    change API code to include Authorization header with relevant jwt

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,20 +1,46 @@
 import axios from 'axios';
+import { Auth } from "aws-amplify";
 import md5 from 'md5';
+
+axios.interceptors.request.use(request => {
+  console.log('Starting Request', request)
+  return request
+})
+
+axios.interceptors.response.use(response => {
+  console.log('Response:', response)
+  return response
+})
 
 class Api {
   constructor() {
     this.baseURL = 'https://cn8pc7plbl.execute-api.eu-west-1.amazonaws.com/staging/';
+    // this.baseURL = 'https://cn8pc7plbl.execute-api.eu-west-1.amazonaws.com/staging-auth/';
+    
     this.config = {
-      headers: {'Content-Type': 'application/json', 'x-api-key': 'inawDBjEa12HYpdIvkUDpaxAcdKA3l4Da7aWcTr7'}
+      headers: {
+        'Content-Type': 'application/json', 
+        'x-api-key': 'inawDBjEa12HYpdIvkUDpaxAcdKA3l4Da7aWcTr7'
+      }
     }
+  }
+  
+  async __getConfigWithAuthHeader() {
+    const headers = {...this.config.headers, 
+      'Authorization': `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}` 
+    }
+    return {...this.config, headers: headers}
   }
 
   async updateRecommendation(patient) {
-    return axios.post(this.baseURL + 'getPatientModelRecommendation', patient.toServerModel(), this.config).then(a => a.data.recommendation);
+    const config = await this.__getConfigWithAuthHeader();
+    
+    return axios.post(this.baseURL + 'getPatientModelRecommendation', patient.toServerModel(), config).then(a => a.data.recommendation);
   }
 
   async getUserInfo(id) {
-    return axios.post(this.baseURL + 'getRecordsByPatient', {patient_id: md5(id)}, this.config).then(a => Object.values(a.data || {})[0]);
+    const config = await this.__getConfigWithAuthHeader();
+    return axios.post(this.baseURL + 'getRecordsByPatient', {patient_id: md5(id)}, config).then(a => Object.values(a.data || {})[0]);
   }
 }
 
